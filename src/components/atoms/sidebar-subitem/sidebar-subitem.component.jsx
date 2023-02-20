863cff6b3297edb416a2701bd69ddd64a32aa4d4
CoderChirag <chirag0174.be20@chitkara.edu.in>
2023-02-21 02:59:39 +0530

    Create SidebarSubItem Component

---
+++ src/components/atoms/sidebar-subitem/sidebar-subitem.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Grid, Typography } from '@mui/material';
+
+const SidebarSubItem = ({ active, title }) => {
+	return (
+		<Grid
+			container
+			alignItems='center'
+			sx={{
+				padding: '5px 35px',
+				backgroundColor: active ? '#eff5fe' : 'inherit',
+				'&:hover': {
+					backgroundColor: '#eff5fe',
+				},
+			}}
+		>
+			<Typography
+				sx={{
+					fontSize: '0.9rem',
+					color: '#828282',
+				}}
+			>
+				{title}
+			</Typography>
+		</Grid>
+	);
+};
+
+export default SidebarSubItem;
